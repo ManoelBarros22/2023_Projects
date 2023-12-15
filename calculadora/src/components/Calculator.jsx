30edb4b7f76a38323998a2a8ab70eced829d9510
Manoel Barros <mbarros962@hotmail.com>
2023-12-15 09:28:18 -0300

    atualização

--- calculadora/src/components/Calculator.jsx
+++ calculadora/src/components/Calculator.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+{/* import React from 'react'
+import { useState } from 'react' */}
+import "./Calculator.css"
 
 const Calculator = () => {
-  return <div>Calculator</div>
+    const keypadNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const operations = ['+', '-', '*', '/'];
+
+  return (
+    <div className='calculator'>
+    <div className="complete-operation"></div>
+    <div className="display"></div>
+    <div className="buttons">
+        <button>AC</button>
+        {keypadNumbers.map((num) => (
+            <button key={num}>{num}</button>
+        ))}
+        {operations.map((operation) => (
+            <button key={operation}>{operation}</button>
+        ))}
+        <button>=</button>
+    </div>
+  </div>
+  )
 }
 
 export default Calculator
