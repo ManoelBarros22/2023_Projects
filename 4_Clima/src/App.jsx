c058c69bb72ee7835288eef49e0a860306248872
Manoel Barros <mbarros962@hotmail.com>
2023-12-17 12:40:30 -0300

    update clima

--- 4_Clima/src/App.jsx
+++ 4_Clima/src/App.jsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+import ClimaAtual from "./components/ClimaAtual";
+import Previsao from "./components/Previsao";
+import Busca from "./components/Busca";
+
+import { ClimaContainer, Titulo } from "./AppStyles";
+
+const App = () => {
+  const [cidade, setCidade] = useState("");
+  const [clima, setClima] = useState(null);
+  const [previsao, setPrevisao] = useState([]);
+
+  const apiKey = import.meta.env.VITE_API_KEY || "";
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      const resposta = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric&lang=pt_br`
+      );
+      setCidade(resposta.data.name);
+      setClima(resposta.data);
+    });
+  }, [apiKey]);
+
+  const buscarClima = async () => {
+    try {
+      const respostaClima = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`
+      );
+      const respostaPrevisao = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`
+      );
+
+      respostaClima.data.main.temp = (respostaClima.data.main.temp - 32) * (5 / 9);
+
+      setClima(respostaClima.data);
+      setPrevisao(respostaPrevisao.data.list.slice(0, 5)); // Obter as 5 primeiras previsões
+    } catch (error) {
+      console.error("Erro ao buscar dados do clima:", error);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ClimaContainer>
+      <Titulo>Condições Climáticas</Titulo>
+      <Busca cidade={cidade} setCidade={setCidade} buscarClima={buscarClima} />
+      {clima && <ClimaAtual clima={clima} />}
+      {previsao.length > 0 && <Previsao previsoes={previsao} />}
+    </ClimaContainer>
+  );
+};
 
-export default App
+export default App;
