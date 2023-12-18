721dec6a3f06fa3a3552909b56ed0ef53ff4425c
Manoel Barros <mbarros962@hotmail.com>
2023-12-18 08:47:10 -0300

    Update App.jsx

--- 5_citações/src/App.jsx
+++ 5_citações/src/App.jsx
@@ -1,12 +1,22 @@
+import React, { useState } from "react";
+import Citacao from "./components/Citacao";
+import citacoes from "./data";
 
 function App() {
+  const [indice, setIndice] = useState(0);
 
+  function proximaCitacao() {
+    setIndice((indiceAtual) => (indiceAtual + 1) % citacoes.length);
+  }
 
   return (
-    <>
-      <h1>Testando bootstrap</h1>
-    </>
-  )
+    <div className="container mt-5">
+      <Citacao texto={citacoes[indice].texto} autor={citacoes[indice].autor} />
+      <button className="btn btn-success mt-2" onClick={proximaCitacao}>
+        Próxima Citação
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
