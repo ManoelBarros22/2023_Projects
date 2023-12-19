22b7d387d8a13cf2eddaf9ac5c102eba738fdb21
Manoel Barros <mbarros962@hotmail.com>
2023-12-19 10:11:08 -0300

    groceries update

--- 0_groceries/src/components/Content.jsx
+++ 0_groceries/src/components/Content.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react'
+
 const Content = () => {
+    const [name, setName] = useState('Manoel')
     const handleNameChange = () => {
-        const names = ['MP', 'Lucy', 'Rod']
-        const int = Math.floor(Math.random() * 3)
-        return names[int]
+        const names = ['MP', 'Lucy', 'Rod', 'Manoel']
+        const int = Math.floor(Math.random() * 4)
+         setName(names[int])
     }
+
+ 
+
+    const handleClick2 = () => {
+    }
+
+    const handleClick3 = () => {
+    }
+
   return (
     <main>
       <p>
-        Hello {handleNameChange()}!
+        Hello {name}!
       </p>
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={() => handleClick2('Manoel')}>Click it2</button>
+      <button onClick={(e) => handleClick3(e)}>Click it3</button>
     </main>
   )
 }
