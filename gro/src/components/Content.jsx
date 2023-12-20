71e17027f3fb3f4b608b4c9b697e61cad8059540
Manoel Barros <mbarros962@hotmail.com>
2023-12-20 08:42:57 -0300

    grocery update

---
+++ gro/src/components/Content.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+
+const Content = () => {
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Item 2"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Item 3"
+        }
+    ]);
+
+    const handleCheck = (id) => {
+        const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
+    const handleDelete = (id) => {
+        const listItems = items.filter((item) => item.id !== id);
+        setItems(listItems);
+        localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+    }
+
+    return (
+        <main>       
+            <ul>
+                {items.map((item) => (
+                    <li className="item" key={item.id}>
+                        <input
+                            type="checkbox"                            
+                            checked={item.checked}
+                        />
+                        <label>{item.item}</label>
+                        <FaTrashAlt 
+                            role="button" 
+                            tabIndex="0" 
+                        />                      
+                    </li>
+                ))}
+            </ul>
+        </main>
+    )
+}
+
+export default Content
