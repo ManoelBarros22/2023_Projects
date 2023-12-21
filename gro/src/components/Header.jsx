470468eb0fa8272bd380d14c9c19e8a788c7c52b
Manoel Barros <mbarros962@hotmail.com>
2023-12-21 09:46:32 -0300

    Groceries update

---
+++ gro/src/components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = (props) => {
+
+  return (
+    <header>
+      <h1>{props.title}</h1>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: "Default Title"
+}
+
+
+export default Header
