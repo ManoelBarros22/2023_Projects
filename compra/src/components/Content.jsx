a3e1d0eb8688a12f3c56cd405440ab57027217b0
Manoel Barros <mbarros962@hotmail.com>
2023-12-23 11:08:58 -0300

    Update Content.jsx

--- compra/src/components/Content.jsx
+++ compra/src/components/Content.jsx
@@ -3,7 +3,7 @@ const Content = () => {
   return (
     <main>
     <p>Olá {handleNameChange()} !</p>
-    <button onClick={handleClick}>Click It</button>
+    <button onClick={handleClick000}>Click It</button>
     </main>
   )
 }
