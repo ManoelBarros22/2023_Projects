13d1e041940d7f1d56963554d1316eeaa35de8d2
Manoel Barros <mbarros962@hotmail.com>
2023-12-23 06:32:28 -0300

    Update compras

--- compras/src/App.jsx
+++ compras/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { handleNameChange } from '../src/utils'
 import './App.css'
 
 function App() {
@@ -17,19 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>{name}</p>
+    <p>Oi {handleNameChange()}!</p>
     </>
   )
 }
---
+++ compras/src/utils/index.js
@@ -0,0 +1,5 @@
+export const handleNameChange = () => {
+    const names = ['Manoel', 'MP', 'Lucy', 'Rod', 'Kay']
+    const int = Math.floor(Math.random() * 5)
+    return names[int]
+}
