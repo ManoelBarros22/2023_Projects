b0df5fbd32d0ef3fb24a67c217bd9cc0fa183bcf
Manoel Barros <mbarros962@hotmail.com>
2023-12-25 10:11:12 -0300

    Update App.jsx

--- 0_groceries/src/App.jsx
+++ 0_groceries/src/App.jsx
@@ -29,26 +29,28 @@ const setAndSaveItems = (newItems) => {
 }
 
 const addItem = (item) => {
-  const id = items.length ? items[items.length - 1].id + 1 : 1
-  const myNewItem = { id, checked: false, item }
-  const ListItems = [...items, myNewItem]
+  const id = items.length ? items[items.length - 1].id + 1 : 1;
+  const myNewItem = { id, checked: false, item };
+  const listItems = [...items, myNewItem];
+  setAndSaveItems(listItems);
 }
 
 const handleCheck = (id) => {
   const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-  setItems(listItems);
-  localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  setAndSaveItems(listItems)
+ 
 }
 
 const handleDelete = (id) => {
   const listItems = items.filter((item) => item.id !== id);
-  setItems(listItems);
-  localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  setAndSaveItems(listItems);
+  
 }
 
 const handleSubmit = (e) => {
   e.preventDefault()
   if (!newItem) return
+  addItem(newItem)
   setNewItem('')
 }
 
