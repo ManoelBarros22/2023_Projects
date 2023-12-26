bfa486a83be800eae640b85586622f46da62f86d
Manoel Barros <mbarros962@hotmail.com>
2023-12-26 09:12:05 -0300

    App update

--- 0_chalenge/src/App.js
+++ 0_chalenge/src/App.js
@@ -1,13 +1,16 @@
 import Input from "./components/Input";
 import Square from "./components/Square";
-
+import { useState } from 'react'
 
 
 function App() {
+  const [colorValue, setColorValue] = useState('')
   return (
     <div className="App">
-      <Square />
-      <Input />
+      <Square colorValue={colorValue} />
+      <Input
+        colorValue={colorValue}
+      />
     </div>
   );
 }
--- 0_chalenge/src/components/Input.js
+++ 0_chalenge/src/components/Input.js
@@ -1,17 +1,17 @@
 const Input = ({ colorValue, setColorValue }) => {
-  return (
+    return (
     <form onSubmit={(e) => e.preventDefault()}>
-      <label>Add Color Name:</label>
-      <input
-        autoFocus
-        type="text"
-        placeholder="Add color name"
-        required
-        value={colorValue}
-        onChange={(e) => setColorValue(e.target.value)}
-      />
+        <label>Add Color Name:</label>
+        <input
+            autoFocus
+            type="text"
+            placeholder="Add color name"
+            required
+            value={colorValue}
+            onChange={(e) => setColorValue(e.target.value)}
+        />
     </form>
-  )
+    )
 }
 
 export default Input
