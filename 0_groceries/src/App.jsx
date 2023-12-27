3461c9bb46ca54c5ef37917df914828eaf2c3c73
Manoel Barros <mbarros962@hotmail.com>
2023-12-27 11:17:48 -0300

    Update App.jsx

--- 0_groceries/src/App.jsx
+++ 0_groceries/src/App.jsx
@@ -11,30 +11,25 @@ const [newItem, setNewItem] = useState('')
 const [search, setSearch] = useState('')
 
 useEffect(() => {
-  localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  localStorage.setItem('shoppinglist', JSON.stringify(items));
 },[items])
-
-const setAndSaveItems = (newItems) => {
-  setItems(newItems);
-  
-}
 
 const addItem = (item) => {
   const id = items.length ? items[items.length - 1].id + 1 : 1;
   const myNewItem = { id, checked: false, item };
   const listItems = [...items, myNewItem];
-  setAndSaveItems(listItems);
+  setItems(listItems);
 }
 
 const handleCheck = (id) => {
   const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-  setAndSaveItems(listItems)
+  setItems(listItems)
  
 }
 
 const handleDelete = (id) => {
   const listItems = items.filter((item) => item.id !== id);
-  setAndSaveItems(listItems);
+  setItems(listItems);
   
 }
 
